4612f78f4c39e870f75a285aa6009eeff6b12f45
yuichi matsuura <y-matuura@pinetreeonline.net>
2025-08-31 20:22:43 +0900

    refactor: RAG関連の数値（chunk_size, chunk_overlap, k）を定数化し、マジックナンバー排除

--- initialize.py
+++ initialize.py
@@ -19,6 +19,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 import constants as ct
+import constants as ct
 
 
 ############################################################
@@ -123,8 +124,8 @@ def initialize_retriever():
     
     # チャンク分割用のオブジェクトを作成
     text_splitter = CharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50,
+        chunk_size=ct.CHUNK_SIZE,
+        chunk_overlap=ct.CHUNK_OVERLAP,
         separator="\n"
     )
 
@@ -135,7 +136,7 @@ def initialize_retriever():
     db = Chroma.from_documents(splitted_docs, embedding=embeddings)
 
     # ベクターストアを検索するRetrieverの作成
-    st.session_state.retriever = db.as_retriever(search_kwargs={"k": 5})
+    st.session_state.retriever = db.as_retriever(search_kwargs={"k": ct.RAG_NUM_RELATED_DOCS})
 
 
 def initialize_session_state():
